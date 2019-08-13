c4a550809ba01b2babddcda4c8c88b72c869f19b
vikasjagdale92 <vikasjagdale92@gmail.com>
2019-08-13 14:30:24 +0530

    frist commit on branch two

---
+++ backend/models/BranchTwo.js
@@ -0,0 +1,16 @@
+//BranchTwo.js
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BranchTwo = new Schema({
+  branchName: {
+    type: String
+  },
+  createdAt: {
+    type: Date
+  }
+});
+
+const branch = mongoose.model("BranchTwo", BranchTwo);
+module.exports = branch;
