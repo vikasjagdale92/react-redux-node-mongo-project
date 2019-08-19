962134555820431dad2f27028844c7776b698765
vikasjagdale92 <vikasjagdale92@gmail.com>
2019-08-19 15:49:42 +0530

    second commit on b-2

--- backend/models/BranchTwo.js
+++ backend/models/BranchTwo.js
@@ -1,5 +1,6 @@
 //BranchTwo.js
 //commit 1--------------yeeeeeeeeeeeeeeeeeeeee yeeeeeeeeeeee
+//commit 2 ------------ yeeeeeeeeeeeee  yeeeeeeeeeeeeee
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
