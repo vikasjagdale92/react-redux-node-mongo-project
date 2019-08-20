8f9dac5b460d5bbd3394c5570ce3a4747180e4a1
vikasjagdale92 <vikasjagdale92@gmail.com>
2019-08-20 14:39:10 +0530

    first new fresh commit on branch-B1

---
+++ backend/models/BranchB1.js
@@ -0,0 +1,15 @@
+//BranchB1.js
+//First Commit
+//2nd Commit.
+
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BranchB1 = new Schema({
+  branchName: {
+    type: String
+  }
+});
+
+const branchB1 = mongoose.model("branchB1", BranchB1);
+module.exports(branchB1);
